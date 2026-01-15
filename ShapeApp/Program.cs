5be0d6744fa65357ad2c6dd9e497dc78c68c64da
Liburn <krasniqiliburn@gmail.com>
2026-01-15 14:28:01 +0100

    first engine

---
+++ ShapeApp/Program.cs
@@ -0,0 +1,165 @@
+﻿using Raylib_cs;
+using System;
+using System.Numerics;
+using ShapeLibrary;
+using static Raylib_cs.Raylib;  // Add this line
+
+class Program
+{
+    const int screenWidth = 800;
+    const int screenHeight = screenWidth;
+    const int universeCenter = 2;
+
+    
+
+    static Vector2 onScreen(Vector2 point){
+        // -1..1 => 0..2 => 0..1 => 0..w/h 
+        // type('Obj', (object,), { 'x' : (p.x + 1)/2 * width, 'y' : (p.y + 1)/2 * height})()
+
+        return new Vector2(((point.X + 1)/2) * screenWidth,
+                             (1- (point.Y + 1)/2) * screenHeight);
+    }
+
+    static Vector2 project(Vector3 point){
+        //(x,y,z) -> 3d point behind screen
+
+        // # points onScreened on screen
+        // x' = x/z
+        // y' = y/z
+        
+        return new Vector2(point.X/point.Z ,point.Y/point.Z);
+    }
+
+    static Vector3 translate_z(Vector3 point, float dz){
+        // dz ~ delta z
+        return new Vector3(point.X ,point.Y, point.Z + dz);
+    }
+
+    static void point(Vector2 point){
+        float size = 10.0f;
+        DrawRectangle((int)(point.X - (size/2)),(int)(point.Y  - (size/2)), (int)size, (int)size, Color.Green);
+    }
+    
+    static void line(Vector2 startPos, Vector2 endPos){
+
+        DrawLineV(startPos, endPos, Color.Green);
+    }
+
+    static void drawShape(float [][] vectors, int [][] faces){
+        foreach(int [] i in faces){
+                    for(int j = 0; j<i.Length; j++){
+                    float [] a = vectors[i [(j+1)% i.Length ]];
+                    float [] b = vectors[i [(j+2)% i.Length ]];
+                    Vector3 A = new Vector3(a[0], a[1], a[2]);  
+                    Vector3 B = new Vector3(b[0], b[1], b[2]);
+                     line(
+                        onScreen(project(translate_z(A, universeCenter))),
+                        onScreen(project(translate_z(B, universeCenter)))
+                    );
+                    }
+                }
+    }
+
+    static void drawShapeObj(Shape shape, double angle){
+        foreach(int [] i in shape.faces){
+                    for(int j = 0; j<i.Length; j++){
+                    Vector3 A = shape.vectors[i [(j+1)% i.Length ]];
+                    Vector3 B = shape.vectors[i [(j+2)% i.Length ]];
+
+                     line(
+                        onScreen(project(translate_z(rotate_xz(A, angle), universeCenter))),
+                        onScreen(project(translate_z(rotate_xz(B, angle), universeCenter)))
+                    );
+                    }
+                }
+    }
+
+    public static Vector3 rotate_xz(Vector3 point, double angle){
+
+    float C =(float) Math.Cos(angle);
+    float S =(float) Math.Sin(angle);
+
+    // \(x^{\prime }=x\cos \theta +z\sin \theta \)
+
+    // \(z^{\prime }=-x\sin \theta +z\cos \theta \)
+
+    return new Vector3(
+        (point.X*C) + (point.Z*S),
+        point.Y,
+        (-(point.X*S)) + (point.Z*C)
+    );
+    }
+
+
+    static void Main()
+    {
+        Shape penger = Parser.readShape("Penger");
+        Shape cube = Parser.readShape("Cube");
+        int currentFps = 60;
+        double angle = 80;
+
+        InitWindow(screenWidth, screenHeight, "3D engine");
+
+        SetTargetFPS(currentFps);
+        SetWindowPosition(500, -400); // to place on second monitor
+
+        SetExitKey(KeyboardKey.Null);
+
+        Vector2 deltaCircle = new Vector2(0, (float)screenHeight/3.0f);
+
+        const float speed = 10.0f;
+        const float circleRadius = 32.0f;
+
+        while (!WindowShouldClose())
+        {
+            if (deltaCircle.X > screenWidth) deltaCircle.X = 0;
+            deltaCircle.X += GetFrameTime()*6.0f*speed;
+
+            // Console.WriteLine(deltaCircle.X);
+            angle +=  Math.PI * (1/(double)currentFps * 0.5d);
+            // Console.WriteLine(angle);
+
+
+
+            BeginDrawing();
+                ClearBackground(Color.Black);
+                // point(onScreen(project(new Vector3(0, 0, 1))));
+                // DrawCircleV(deltaCircle, circleRadius, Color.Green);
+                // drawShape(VS, FS);
+                drawShapeObj(penger, angle);
+                drawShapeObj(cube, angle);
+                // werk on here
+                // foreach(int [] i in FS){
+                //     for(int j = 0; j<i.Length; j++){
+                //     float [] a = VS[i [(j+1)% i.Length ]];
+                //     float [] b = VS[i [(j+2)% i.Length ]];
+                //     Vector3 A = new Vector3(a[0], a[1], a[2]);  
+                //     Vector3 B = new Vector3(b[0], b[1], b[2]);
+                //      line(
+                //         onScreen(project(translate_z(A, universeCenter))),
+                //         onScreen(project(translate_z(B, universeCenter)))
+                //     );
+                //     }
+                // }
+
+                // foreach(float [] v in VS){
+                //     point(onScreen(project(translate_z(new Vector3(v[0], v[1], v[2]), universeCenter))));
+                // }                
+                // for v in VSB:
+                //     pointRed(onScreen(project(translate_z(rotate_xz(v, angle), dz))))
+
+                // point(onScreen(project(translate_z(new Vector3(VS[0][0],VS[0][1],VS[0][2]), universeCenter))));
+                // point(onScreen(project(translate_z(new Vector3(VS[1][0],VS[1][1],VS[1][2]), universeCenter))));
+                
+                // DrawLineV(onScreen(project(translate_z(new Vector3(VS[0][0],VS[0][1],VS[0][2]), universeCenter))),
+                //          onScreen(project(translate_z(new Vector3(VS[2][0],VS[2][1],VS[2][2]), universeCenter))), Color.Green);
+                // line(
+                //     onScreen(project(translate_z(new Vector3(VS[0][0],VS[0][1],VS[0][2]), universeCenter))),
+                //     onScreen(project(translate_z(new Vector3(VS[1][0],VS[1][1],VS[1][2]), universeCenter)))
+                //     );
+            EndDrawing();
+        }
+
+        CloseWindow();
+    }
+}
---
+++ ShapeLibrary/Shape.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization; // Required for CultureInfo, dont really need it i think
+using System.Numerics;
+using Raylib_cs;
+using static Raylib_cs.Raylib;  // Add this line
+
+namespace ShapeLibrary;
+
+public class Shape 
+{
+    public string name;
+    public Vector3 [] vectors;
+    public int[][] faces;
+    public Vector3 center;
+
+    public Shape(string name, Vector3 [] vectors, int[][] faces, Vector3? center = null)
+    {
+        this.name = name;
+        this.vectors = vectors;
+        this.faces = faces;
+        this.center = center ?? new Vector3(0, 0, 0);  // Use default if null
+    }
+}
+
+public class Parser{
+
+    public static Shape readShape(string shape){
+
+        string filePath = $"/Users/liburn/Desktop/curr/Ritech-Internship/test2/Shapes/{shape}.txt";
+
+        if (File.Exists(filePath))
+        {
+            try
+            {
+                string [] fileContents = File.ReadAllLines(filePath);
+
+                List<Vector3> vectors = new List<Vector3>();
+                List<int[]> faces = new List<int[]>();
+
+
+                Console.WriteLine("Here are the contents of the file:");
+                // Console.WriteLine(fileContents);
+                Console.WriteLine(fileContents.Length);
+
+                for (int i = 0; i < fileContents.Length;  i++){
+                    if(fileContents[i].Contains("vectors") && !fileContents[i].Contains("paths")){
+                        Console.WriteLine(fileContents[i]);
+                        for(int j = i+1; j < fileContents.Length; j++){
+                            if(fileContents[j].Contains("]")) {
+                             i=j+1;
+                             break;
+                             }
+                            float [] vector = fileContents[j].Trim().Split(',').Select(s => float.Parse(s)).ToArray();
+                            
+                            vectors.Add(new Vector3(vector[0], vector[1], vector[2]));
+
+                        }
+                    }
+
+                    if(fileContents[i].Contains("vector_paths")){
+                        Console.WriteLine(fileContents[i]);
+                        for(int j = i+1; j < fileContents.Length; j++){
+                            if(fileContents[j].Equals("]")) break;
+
+                            char [] trim = {' ', '[',']', ','};
+
+                            int [] vector_path = fileContents[j].Trim(trim).Split(',').Select(s => int.Parse(s)).ToArray();
+
+                            faces.Add(vector_path);
+                        }
+                    }
+                }
+                return new Shape(shape, vectors.ToArray(), faces.ToArray(), null);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"An error occurred: {e.Message}");
+                return null;
+            }
+        }
+        else
+        {
+            Console.WriteLine($"File not found: {filePath}");
+            return null;
+        }
+    }
+}
